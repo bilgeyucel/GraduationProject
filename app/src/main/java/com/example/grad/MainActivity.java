04b0e4a7eed743a118ce744d9d3c0ff4e3186e2c
elfistanli <elifsozefe@hotmail.com>
2019-04-21 15:00:29 +0300

    özefe: save full size image

--- app/src/main/java/com/example/grad/MainActivity.java
+++ app/src/main/java/com/example/grad/MainActivity.java
@@ -132,34 +132,20 @@ public class MainActivity extends AppCompatActivity {
            Bundle extras = data.getExtras();
            Bitmap photo = (Bitmap) extras.get("data");
            image_photo.setImageBitmap(photo);
+
+           // http things
            String s = encodeToBase64(photo,Bitmap.CompressFormat.PNG,100);
            int length = s.length();
-            Log.d("COMPRESS", Integer.toString(length));
+           Log.d("COMPRESS", Integer.toString(length));
            POST_PARAMS = POST_PARAMS + s;
-//           Log.d("COMPRESS", POST_PARAMS);
-           saveToInternalStorage(photo);
 
-//            loadImageFromStorage("data/data/com.example.grad/app_imageDir");
-             new DownloadImageTask().execute("http://134.209.226.2:5000/api/photoSend");
-//            viewModel.setRating(data.getStringExtra(KEY));
-
-
-
-            //uploadSelectedImageToServer();
-            // request cagirma
-//            try {
-//                sendGET();
-//                Log.d("grad", "gonderdi");
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                Log.d("grad", "gonderemedi");
-//            }
+//         loadImageFromStorage("data/data/com.example.grad/app_imageDir");
+//         new DownloadImageTask().execute("http://134.209.226.2:5000/api/photoSend");
         }
     }
 
     private String saveToInternalStorage(Bitmap bitmapImage){
         ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        // path to /data/data/yourapp/app_data/imageDir
         File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
         // Create imageDir
         File mypath = new File(directory,"sudoku_unsolved.jpg");
@@ -236,84 +222,6 @@ public class MainActivity extends AppCompatActivity {
 //        tw.setText(path);
 //
 //    }
-
-
-    // https://inducesmile.com/android/android-upload-image-to-remote-server-and-download-image-from-remote-server-to-external-storage/
-//    private void uploadSelectedImageToServer() {
-//        // make a post request to the server
-//        StringRequest stringPostRequest = new StringRequest(Request.Method.POST, Helper.PATH_TO_SERVER_IMAGE_UPLOAD, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                //Toast.makeText(UploadImageActivity.this, response.toString(), Toast.LENGTH_LONG).show();
-//                GsonBuilder builder = new GsonBuilder();
-//                Gson gson = builder.create();
-//                imageObject = gson.fromJson(response, ServerImageObject.class);
-//                if (null == imageObject) {
-//                    Toast.makeText(MainActivity.this, "Something went wrong and file was not uploaded in the server", Toast.LENGTH_LONG).show();
-//                    return;
-//                } else {
-//                    if (imageObject.getSuccess().equals("0")) {
-//                        // something went wrong
-//                        Toast.makeText(MainActivity.this, "Something went wrong and file was not uploaded in the server", Toast.LENGTH_LONG).show();
-//                        return;
-//                    } else {
-//                        //Successful upload
-//                        long timeAfterUpload = System.currentTimeMillis();
-//                        double timeDifferenceInSeconds = (double)(timeAfterUpload - timeBeforeUpload) / 1000;
-//                        double fileSizeInKiloByte = (double)(fileSize / 1024);
-//                        double fileUploadRate = (timeDifferenceInSeconds / fileSizeInKiloByte);
-//                        String result = String.format("%.2f", fileUploadRate);
-//                        circularProgress.setTitleText(result + "Kb/s");
-//                        circularProgress.setSubtitleText("Upload speed");
-//                        Toast.makeText(MainActivity.this, "Your image was successfully uploaded to the server", Toast.LENGTH_LONG).show();
-//                    }
-//                }
-//            }
-//        },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_LONG).show();
-//                    }
-//                }) {
-//            @Override
-//            protected Map<String, String> getParams() {
-//                Map<String, String> params = new HashMap<String, String>();
-//                params.put(Helper.IMAGE_STRING, encodedString);
-//                params.put(Helper.IMAGE_FILENAME, filename);
-//                return params;
-//            }
-//        };
-//        queue.add(stringPostRequest);
-//    }
-
-//    public static void sendGET() throws IOException {
-//        String url = "https://www.google.com"; //+ encodedString;
-//        URL obj = new URL(url);
-//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-//        con.setRequestMethod("GET");
-//        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-//        int responseCode = con.getResponseCode();
-//        System.out.println("GET Response Code :: " + responseCode);
-//        if (responseCode == HttpURLConnection.HTTP_OK) { // success
-//            BufferedReader in = new BufferedReader(new InputStreamReader(
-//                    con.getInputStream()));
-//            String inputLine;
-//            StringBuffer response = new StringBuffer();
-//
-//            while ((inputLine = in.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//            in.close();
-//
-//            // print result
-//            System.out.println(response.toString());
-//        } else {
-//            System.out.println("GET request not worked");
-//        }
-//
-//    }
-
 
     private class DownloadImageTask extends AsyncTask <String, Void, String> {
         protected String doInBackground(String... urls) {
