bc3bd58343fb17869e5b25ab545d601720b09f50
bilgeyucel <bilgeyucel@sabanciuniv.edu>
2019-04-30 11:17:59 +0300

    camera high quality photo

--- app/src/main/java/com/example/grad/MainActivity.java
+++ app/src/main/java/com/example/grad/MainActivity.java
@@ -7,9 +7,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
@@ -43,6 +46,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import android.arch.lifecycle.ViewModelProviders;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -69,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
     private static String POST_PARAMS = "image=";
     private String solution = "You have not taken a photo yet";
     TextView tw;
+    public String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,6 +113,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        take_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
     }
 
     //check if the user has a camera
@@ -126,16 +139,59 @@ public class MainActivity extends AppCompatActivity {
         image.compress(compressFormat, quality, byteArrayOS);
         return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
     }
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                Log.d("grad", photoFile.toString());
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                "yenihal",  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+        File image = new File(storageDir +"/yenihal.png");
+
+
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     //if you want to return the image taken
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             // get the photo
-            Bundle extras = data.getExtras();
-            Bitmap photo = (Bitmap) extras.get("data");
-            image_photo.setImageBitmap(photo);
-            saveToInternalStorage(photo);
+//            Bundle extras = data.getExtras();
+//            Bitmap photo = (Bitmap) extras.get("data");
+//            image_photo.setImageBitmap(photo);
+//            saveToInternalStorage(photo);
             // http things
 //            String s = encodeToBase64(photo,Bitmap.CompressFormat.PNG,100);
 //            int length = s.length();
@@ -337,7 +393,8 @@ public class MainActivity extends AppCompatActivity {
 //                fis = new FileInputStream(f);
                 // use the get method , if you are using android remember to remove "file://" and use only the relative path
 //                sftp.put(new FileInputStream(f),f.getName());
-                sftp.put("data/user/0/com.example.grad/app_imageDir/sudoku_unsolved.jpg","yenihal.jpg");
+//                sftp.put("data/user/0/com.example.grad/app_imageDir/yenihal.png","bilge.jpg");
+                sftp.put("/storage/emulated/0/Android/data/com.example.grad/files/Pictures/yenihal.png", "yenihal.jpg");
                 Log.d("grad", "sftp bayagi girmis");
                 Boolean success = true;
 
